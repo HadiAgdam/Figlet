b1b3ab8ab0fbf624fe701a5eaad59ddf4c9b69d0
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-08-15 17:01:22 +0330

    added argv to main

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import sys
 from figlet import convert
 
 
-print(convert("hello world"))
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please provide the text as a command-line argument.")
+        sys.exit(1)
 
+    text = " ".join(sys.argv[1:])  # Join all arguments after the script name as the text input
+    print(convert(text))
